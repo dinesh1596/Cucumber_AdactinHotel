87b72cedd915f627b0c180eab8ebcbf096150a75
dinesh1596 <114136389+dinesh1596@users.noreply.github.com>
2024-03-28 14:04:59 +0530

    Update TestRunnerClass.java

--- src/test/java/com/runner/TestRunnerClass.java
+++ src/test/java/com/runner/TestRunnerClass.java
@@ -14,7 +14,7 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags="@BookHotel",dryRun= false, features="src\\test\\resources\\features", glue="com.stepdefinition",
+@CucumberOptions(tags="",dryRun= false, features="src\\test\\resources\\features", glue="com.stepdefinition",
 publish= false,monochrome= true, plugin= {"pretty","json:target//Output.json"},snippets=SnippetType.CAMELCASE )
 
 public class TestRunnerClass extends BaseClass {
